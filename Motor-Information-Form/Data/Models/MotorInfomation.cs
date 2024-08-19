a620505fd68ba3611c5e29f6731fede004ae7c53
Josh Hill <148567526+Sahujo-Hill@users.noreply.github.com>
2024-08-19 21:10:00 +0100

    Initial Upload
    API interaction added
    Bootstrap added
    Built generic Mk1 interface

---
+++ Motor-Information-Form/Data/Models/MotorInfomation.cs
@@ -0,0 +1,16 @@
+﻿namespace Motor_Information_Form.Data.Models
+{
+    public class MotorInformation
+    {
+        public string colour { get; set; }
+        public int engineCapacity { get; set; }
+        public string fuelType { get; set; }
+        public string make { get; set; }
+        public string registrationNumber { get; set; }
+        public string motStatus { get; set; }
+        public string taxStatus { get; set; }
+        public int yearOfManufacture { get; set; }
+        public int revenueWeight { get; set; }
+
+    }
+}
---
+++ Motor-Information-Form/Data/Services/MotorInformationService.cs
@@ -0,0 +1,47 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using System.Text.Json;
+using Motor_Information_Form.Data.Models;
+
+namespace Motor_Information_Form.Data.Services
+{
+    public class MotorInformationService
+    {
+        private static readonly string BaseUrl = "https://driver-vehicle-licensing.api.gov.uk/vehicle-enquiry/v1/vehicles";
+        private readonly HttpClient _httpClient;
+
+        public MotorInformationService(string apiKey)
+        {
+            _httpClient = new HttpClient
+            {
+                BaseAddress = new Uri(BaseUrl)
+            };
+            _httpClient.DefaultRequestHeaders.Accept.Clear();
+            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            _httpClient.DefaultRequestHeaders.Add("x-api-key", apiKey);
+        }
+
+        public async Task<MotorInformation> PostReg(string registrationNumber)
+        {
+            var requestBody = new { registrationNumber = registrationNumber };
+            var response = await _httpClient.PostAsJsonAsync(string.Empty, requestBody);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                var errorContent = await response.Content.ReadAsStringAsync();
+                throw new HttpRequestException($"API request failed with status code {response.StatusCode}, Response: {errorContent}");
+            }
+
+            var motorInformation = await response.Content.ReadFromJsonAsync<MotorInformation>();
+
+            if (motorInformation == null)
+            {
+                throw new HttpRequestException("Deserialization has returned null, please check JSON data.");
+            }
+
+            return motorInformation;
+        }
+    }
+}
